c64a08fa3029c27c8408393487fb37abd74340f7
lukavdplas <lukavdplas@gmail.com>
2023-03-30 11:47:28 +0200

    add counting function

--- analysis/count.py
+++ analysis/count.py
@@ -1,6 +1,14 @@
 from sklearn.feature_extraction.text import CountVectorizer
 
-def make_vectoriser(text):
+def make_vectoriser(texts):
     cv = CountVectorizer()
-    cv.fit(text)
+    cv.fit(texts)
     return cv
+
+def count_tokens(cv: CountVectorizer, texts):
+    '''Return a sparse matrix of term frequencies per text'''
+    return cv.transform(texts)
+
+def total_frequencies(tdm):
+    '''Get total frequencies from a term-document matrix'''
+    return tdm.sum(0)
--- analysis/tests/test_count.py
+++ analysis/tests/test_count.py
@@ -1,3 +1,31 @@
+from analysis.count import *
+
+texts = [
+    'Willem die Madocke maecte,',
+    'Daer hi dicken omme waecte,',
+    'Hem vernoyde so haerde',
+    'Dat die avonture van Reynaerde',
+    'In Dietsche onghemaket bleven',
+    'Die Arnout niet hevet vulscreven',
+    'Dat hi die vijte dede soucken',
+    'Ende hise na den Walschen boucken',
+    'In Dietsche dus hevet begonnen',
+]
 
 def test_vocab():
-    pass
+    cv = make_vectoriser(texts)
+    vocab = list(cv.get_feature_names_out())
+    
+    assert 'hi' in vocab
+    assert all(
+        word == word.lower() for word in vocab
+    )
+
+def test_counts():
+    cv = make_vectoriser(texts)
+    vocab_size = cv.get_feature_names_out().size
+    tdm = count_tokens(cv, texts)
+    assert tdm.shape == (len(texts), vocab_size)
+
+    totals = total_frequencies(tdm)
+    assert totals.shape == (1, vocab_size)
