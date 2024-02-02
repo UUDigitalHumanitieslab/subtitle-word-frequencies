ecaf17f6d93058355ac1da1653c6e9739ee2c591
Luka van der Plas <lukavdplas@mailfence.com>
2024-02-02 12:49:42 +0100

    add docstring

--- vtt/convert_to_plain.py
+++ vtt/convert_to_plain.py
@@ -4,6 +4,15 @@ from .parse_vtt import parse_vtt
 from .filter_metatext import filter_metatext
 
 def export_plain_text(vtt_filename):
+    '''
+    Export a plain text version of a .vtt subtitle file
+
+    Applies `filter_metatext` to filter out non-utterances.
+    
+    Saves the output of `{x}/{y}.vtt` in `{x}/{y}.plain.txt`,
+    with one line per utterance.
+    '''
+
     lines = parse_vtt(vtt_filename)
     filtered_lines = filter_metatext(lines)
 
